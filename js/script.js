0e4bb681841c785f101ea174523150e32e2207f8
Lanthier Chloé <chloedino06@gmail.com>
2025-04-09 15:19:28 -0400

    debut

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const bouton = document.querySelectorAll('.bouton-modale');
+const modale = document.querySelectorAll('modale');
+
+for (let i = 0; index < bouton.length; i++) {
+    const element = bouton[i];
+    document.addEventListener('click', openModale);
+}
+
+function openModale (e) {
+    
+}
